52ebb3f81fa19fad3f91e9ab92b5dd78b4847d02
1DAM - Eduardo Daza Parra <edazap01@iesvjp.es>
2026-09-25 17:45:34 +0200

    ejercicio9

---
+++ ejercicio09/src/main/java/com/mycompany/ejercicio09/Ejercicio09.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio09;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio09 {
+
+    public final static double PI = 3.1416;
+    
+    
+    public static void main(String[] args) {
+        
+        
+        double radio = 3.55;
+        double longitud = 2 * PI * radio;
+    
+    System.out.println("La longitud de una circunferencia cuyo radio vale 3.55 seria igual a " + longitud + " metros. ");
+    }
+}
