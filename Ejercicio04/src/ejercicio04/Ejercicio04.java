0b599bc76e1eeb770ff25bba58ecab28942b0af0
1DAM - Eduardo Daza Parra <edazap01@iesvjp.es>
2026-09-25 17:19:46 +0200

    ejercicio guardado

---
+++ Ejercicio04/src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio04;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        String asignatura = "Programacion";
+        float nota1 = 7.5f;
+        float nota2 = 8.0f;
+
+        float media = (nota1 + nota2) / 2;
+
+        System.out.println(asignatura);
+        System.out.println(nota1);
+        System.out.println(nota2);
+        System.out.println(media);// TODO code application logic here
+    }
+    
+}
