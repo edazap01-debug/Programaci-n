efa864460c258fe1a4444353f756a78fae805f63
1DAM - Eduardo Daza Parra <edazap01@iesvjp.es>
2026-09-25 17:47:42 +0200

    ejercicio23

---
+++ ejercicio23/src/main/java/com/mycompany/ejercicio23/Ejercicio23.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio23;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio23 {
+
+    public static void main(String[] args) {
+        
+        int precio = 2;
+        int unidades = 5;
+        int precioTotal = precio * unidades;
+        
+        System.out.println("Por favor, introduzca el precio del modelo de ordenador que quiere comprar: " + precio + " euros.");
+        System.out.println("¿Cuantas unidades quiere llevarse? " + unidades + " unidades");
+        System.out.println("El precio final de su compra es de " + precioTotal + " euros.");
+    }
+}
